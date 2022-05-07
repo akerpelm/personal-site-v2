580eb912b2c9ee97124ff0239d53188f1b6542ff
akerpelm <akerpelm@u.rochester.edu>
2022-05-06 20:12:31 -0400

    begin assets, intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,23 @@ import "./intro.scss";
 const Intro = () => {
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="img-container">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Alex</h1>
+          <h3>
+            Software<span></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   );
 };
